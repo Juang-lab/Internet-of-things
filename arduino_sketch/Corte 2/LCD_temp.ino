290f5a509a5246f491311ec9131da30808a22111
Juan <jugarc9427@gmail.com>
2025-10-15 21:15:59 -0500

    entrega corte 2

--- arduino_sketch/Corte 2/LCD_temp.ino
+++ arduino_sketch/Corte 2/LCD_temp.ino
@@ -3,7 +3,7 @@
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 const int sensorPin = A0;  
-const int ledPin = 8;       
+const int ledPin = 13;       
 const int motorPin = 9; 
 
 float temperatura = 0;
@@ -22,14 +22,19 @@ void loop() {
   float voltaje = lectura * (5.0 / 1023.0);
   temperatura = (voltaje - 0.5) * 100.0;  
 
+  
   lcd.setCursor(0, 0);
   lcd.print("Temp: ");
   lcd.print(temperatura);
   lcd.print((char)223); 
   lcd.print("C   ");
 
+ 
   if (temperatura <= 10) {
+   
     digitalWrite(motorPin, LOW);
+
+   
     lcd.setCursor(0, 1);
     lcd.print("FRIO: LED Blink ");
     digitalWrite(ledPin, HIGH);
@@ -38,15 +43,19 @@ void loop() {
     delay(500);
 
   } else if (temperatura > 10 && temperatura <= 25) {
+    
     digitalWrite(ledPin, LOW);
     digitalWrite(motorPin, LOW);
+
     lcd.setCursor(0, 1);
     lcd.print("NORMAL          ");
     delay(1000);
 
-  } else {
+  } else if (temperatura >= 26) {
+    
     digitalWrite(ledPin, HIGH);
     digitalWrite(motorPin, HIGH);
+
     lcd.setCursor(0, 1);
     lcd.print("CALOR: Motor ON ");
     delay(1000);
